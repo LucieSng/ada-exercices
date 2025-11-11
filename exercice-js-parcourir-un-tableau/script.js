434a8d7c1aa90dbef6bf4f004cca46c864b934aa
Lucie Seng <lucie.sng@gmail.com>
2025-11-11 16:56:29 +0100

    feat : display HTML

--- exercice-js-parcourir-un-tableau/script.js
+++ exercice-js-parcourir-un-tableau/script.js
@@ -21,3 +21,12 @@ console.log(candidates[7]);
 candidates.forEach((candidate) => {
   console.log(candidate);
 });
+
+// (Bonus) Parcours le tableau candidates pour afficher la liste des candidates dans une page HTML.
+const listCandidates = document.querySelector(".candidates");
+
+candidates.forEach((candidate) => {
+  const listItem = document.createElement("li"); // Création d'un nouvel élément HTML liste, vide pour l'instant
+  listItem.textContent = candidate; // Insérer le nom de la candidate à l'intérieur du <li>.
+  listCandidates.appendChild(listItem); // Ajouter l'élément <li> comme un enfant de l'élément listCandidates, donc à l'intérieur de la liste dans le HTML
+});
