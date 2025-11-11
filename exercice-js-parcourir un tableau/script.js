d1f8685ba0dee512c9376ddf6df9ad65c598c1c7
Lucie Seng <lucie.sng@gmail.com>
2025-11-11 11:19:35 +0100

    add file : step 1 - 3

---
+++ exercice-js-parcourir un tableau/script.js
@@ -0,0 +1,23 @@
+const candidates = [
+  "Le Filip",
+  "Ruby On The Nail",
+  "Leona Winter",
+  "Lula Strega",
+  "Misty Phoenix",
+  "Perseo",
+  "Norma Bell",
+  "Edeha Noire",
+  "Magnetica",
+  "Afrodite Amour",
+];
+
+// Accède au tableau pour afficher dans la console “Le Filip”
+console.log(candidates[0]);
+
+// Accède au tableau pour afficher dans la console “Edeha Noire”
+console.log(candidates[7]);
+
+// Parcours le tableau pour afficher dans la console ligne par ligne chaque nom des candidates
+candidates.forEach((candidate) => {
+  console.log(candidate);
+});
