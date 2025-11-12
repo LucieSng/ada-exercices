b71dbd43ded71cf5483b2acb1b2b445f532dba47
Lucie Seng <lucie.sng@gmail.com>
2025-11-12 15:42:22 +0100

    feat : loader + com

--- exercice-js-liste d'offres/script.js
+++ exercice-js-liste d'offres/script.js
@@ -1,15 +1,24 @@
-// Je déclare me fonction
+// Déclaration de la fonction
 async function fetchOffers() {
-  const response = await fetch(
-    "https://corsproxy.io/?https://codepassport.dev/api/offers"
-  ); // Je fais ma requête avec fetch
-  const offers = await response.json(); // Je convertis ma réponse en json
   const offersContainer = document.querySelector(".offres");
+  offersContainer.innerHTML = "Chargement des données..."; // Afficher le message de chargement
+  try {
+    // Récupérer les données
+    const response = await fetch(
+      "https://corsproxy.io/?https://codepassport.dev/api/offers"
+    ); // Faire la requête avec fetch
+    const offers = await response.json(); // Convertir ma réponse en json
+    offersContainer.innerHTML = ""; // Vider le conteneur avant d'afficher les offres
 
-  for (let i = 0; i < offers.length; i++) {
-    // Je crée une boucle pour tous les éléments
-    offersContainer.innerHTML += `<h2>${offers[i].titre}</h2> <p>${offers[i].description}</p>`; // J'affiche HTML en ajoutant le titre et la description
+    for (let i = 0; i < offers.length; i++) {
+      // Générer le HTML dans une boucle
+      offersContainer.innerHTML += `<h2>${offers[i].titre}</h2> <p>${offers[i].description}</p>`; // J'affiche HTML en ajoutant le titre et la description
+    }
+    return offers;
+  } catch (error) {
+    // Afficher le message d'erreur si quelque chose ne va pas
+    offersContainer.innerHTML = "Une erreur est survenue lors du chargement";
   }
-  return offers;
 }
+
 fetchOffers();
