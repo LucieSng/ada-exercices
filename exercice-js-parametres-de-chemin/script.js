377639e55b534304ee40ac531a6a2052358315d1
Lucie Seng <lucie.sng@gmail.com>
2025-11-13 11:29:31 +0100

    feat : show three info for one user

--- exercice-js-parametres-de-chemin/script.js
+++ exercice-js-parametres-de-chemin/script.js
@@ -1,1 +1,11 @@
 // L’idée, c'est de créer une page web qui affiche les infos d’une donnée en particulier (un post ou une offre par exemple), plutôt qu’afficher la liste entière.
+// Déclaration de la fonction en asynchrone
+async function fetchUsers() {
+  const user = document.querySelector(".user");
+  const response = await fetch("https://dummyjson.com/users/1"); // Récupération des données json
+  const users = await response.json(); // Convertir ma réponse en json
+  // console.log(`${users.firstName} ${users.lastName} ${users.image}`);
+  user.innerHTML += `${users.firstName} ${users.lastName} ${users.email}`;
+}
+
+fetchUsers();
