2eefc1e0a26627e159a0ba749f8302fb8d6152cc
Lucie Seng <lucie.sng@gmail.com>
2025-11-14 15:37:50 +0100

    feat : table check

--- exercice-js-refactoring-javascript/script.js
+++ exercice-js-refactoring-javascript/script.js
@@ -1,18 +1,23 @@
-// Refactoring dans ce code pour qu'il soit plus simple à la relecture
-function number(arr) {
-  let n = 0;
-  for (let i = 0; i < arr.length; i++) {
-    n += arr[i];
+// Refactoring du code pour qu'il soit plus simple à la relecture
+function sumNumber(arr) {
+  // Vérifier si arr est bien un tableau
+  if (!Array.isArray(arr)) {
+    return "Erreur vous devez passer à un tableau de nombres";
   }
-  return n;
+
+  let number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    number += arr[i];
+  }
+  return number;
 }
 
-console.log(number([1, 2, 3, 4])); // Devrait retourner 10
-console.log(number([1, 2])); // Devrait retourner 3
-console.log(number([0])); // Devrait retourner 0
-console.log(number([1, 0])); // Devrait retourner 1
+console.log(sumNumber([1, 2, 3, 4])); // Devrait retourner 10
+console.log(sumNumber([1, 2])); // Devrait retourner 3
+console.log(sumNumber([0])); // Devrait retourner 0
+console.log(sumNumber([1, 0])); // Devrait retourner 1
 
 // Gestion des erreurs
-console.log(f1([1])); // Devrait retourner 1
-console.log("Erreur : Vous devez passer un tableau de nombres"); // Devrait retourner "Erreur: Vous devez passer un tableau de nombres"
-console.log(f1("Huhu")); // Devrait retourner "Erreur: Vous devez passer un tableau de nombres"
+console.log(sumNumber([1])); // Devrait retourner 1
+console.log(sumNumber()); // Devrait retourner "Erreur: Vous devez passer un tableau de nombres"
+console.log(sumNumber("Huhu")); // Devrait retourner "Erreur: Vous devez passer un tableau de nombres"
