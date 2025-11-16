11ea02debe8f100af65cbca5339866e6137c9240
Lucie Seng <lucie.sng@gmail.com>
2025-11-16 13:56:07 +0100

    feat : update data movies with img

--- exercice-js-parametres-de-chemin/script.js
+++ exercice-js-parametres-de-chemin/script.js
@@ -1,9 +1,12 @@
-// L’idée, c'est de créer une page web qui affiche les infos d’une donnée en particulier (un post ou une offre par exemple), plutôt qu’afficher la liste entière.
-// Déclaration de la fonction en asynchrone
-async function fetchUsers() {
-  const user = document.querySelector(".user");
-  const response = await fetch("https://dummyjson.com/users/1"); // Récupération des données json
-  const users = await response.json(); // Convertir ma réponse en json
-  user.innerHTML += `${users.firstName} ${users.lastName} ${users.email}`;
+// L’idée est de créer une page web qui affiche les infos d’une donnée en particulier (un post ou une offre par exemple), plutôt qu’afficher la liste entière.
+
+async function fetchMovies() {
+  const moviesContainer = document.querySelector(".movies");
+  const response = await fetch("https://ghibliapi.vercel.app/films"); // Récupération des données json
+  const data = await response.json(); // Convertir ma réponse en json
+  for (let i = 0; i < 5; i++) {
+    const movie = data[i]; // Création d'une variable movie qui contient le numéro de film [i]
+    moviesContainer.innerHTML += `<div> <h1>${movie.title}</h1> <h2>${movie.director}<h2> <p>${movie.release_date}</p> <img src="${movie.movie_banner}" alt="Affiche de ${movie.title}" width=300px></div>`;
+  }
 }
-fetchUsers();
+fetchMovies();
